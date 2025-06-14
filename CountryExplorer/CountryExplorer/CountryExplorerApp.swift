427178adc4e96408c8e413ce5b18f3a966978e67
FatmaAnwar <43218241+FatmaAnwar@users.noreply.github.com>
2025-06-14 11:05:13 +0300

    Initial project setup using SwiftUI
    - Created a new Xcode project for CountryExplorer
    - SwiftUI lifecycle 
    - iOS 15+ deployment target

---
+++ CountryExplorer/CountryExplorer/CountryExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryExplorerApp.swift
+//  CountryExplorer
+//
+//  Created by Fatma Anwar on 14/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CountryExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountryExplorer/CountryExplorerTests/CountryExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountryExplorerTests.swift
+//  CountryExplorerTests
+//
+//  Created by Fatma Anwar on 14/06/2025.
+//
+
+import Testing
+@testable import CountryExplorer
+
+struct CountryExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
